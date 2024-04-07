eff3ff9b0ad98a03f0015d6ab6fa3e0ce86df061
Juan Carlos <juan.jc971@gmail.com>
2024-04-07 14:36:26 -0600

    cors added

--- app.js
+++ app.js
@@ -7,8 +7,21 @@ const contentsRoutes = require('./routes/contents');
 
 const app = express();
 
+const allowedOrigins = [
+    'http://localhost:3001', // Localhost to dev
+    'https://pruebatecnica-7946d.web.app' // Production
+];
+
 app.use(cors({
-    origin: 'http://localhost:3001'
+    origin: function (origin, callback) {
+        if (!origin) return callback(null, true);
+
+        if (allowedOrigins.indexOf(origin) === -1) {
+            var msg = 'La política de CORS para este sitio no permite acceso desde el origen especificado.';
+            return callback(new Error(msg), false);
+        }
+        return callback(null, true);
+    }
 }));
 
 if (process.env.NODE_ENV !== 'test') {
